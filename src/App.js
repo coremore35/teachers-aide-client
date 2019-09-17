f9d2ab2f7f38fa92d72daed5dae1278e8faeeb0a
Mushnik <cmorrison@Coreys-Mac.local>
2019-09-17 17:25:48 -0400

    commented out baseURL for localhost

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nav, Navbar } from 'react-bootstrap'
 
-// const baseURL = process.env.REACT_APP_BASEURL
-const baseURL = 'http://localhost:3000';
+const baseURL = process.env.REACT_APP_BASE_URL
+// const baseURL = 'http://localhost:3000';
+// const baseURL = 'http://localhost:3000';
 console.log("BASEURL", baseURL)
 
 class App extends React.Component {
